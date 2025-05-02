b578832179e842d9bd9033bb0802b20639660261
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-05-01 18:17:56 -0700

    Registrar usuarios en el login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),  # Ruta para iniciar sesión
     path('logout/', views.logout_view, name='logout'),  # Ruta para cerrar sesión
     path('dashboard/', views.dashboard, name='dashboard'),  # Ruta para el dashboard
+    path('register/', views.register_view, name='register'),
+    path('', views.home, name='home'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
 def login_view(request):
     if request.method == 'POST':
@@ -19,6 +20,21 @@ def login_view(request):
             messages.error(request, 'Usuario o contraseña incorrectos')  # Muestra un mensaje de error si las credenciales son incorrectas
 
     return render(request, 'login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'El nombre de usuario ya existe.')
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'register.html')
 
 @login_required
 def dashboard(request):
